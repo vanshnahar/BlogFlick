972e310c40b2ad8d457d057c007c9b4a8087ab57
Vansh nahar <118673278+vanshnahar@users.noreply.github.com>
2024-09-22 21:26:47 +0530

    first commit

---
+++ src/appwrite/config.js
@@ -0,0 +1,107 @@
+import conf from "../conf/conf.js";
+import { Client,ID,Databases,Storage,Query } from "appwrite";
+
+
+export class Service{
+  client=new Client();
+  Databases;
+  bucket;
+  constructor(){
+    this.client
+    .setEndpoint(conf.appwriteURl)
+    .setProject(conf.appwriteProjectId);
+    this.Databases=new Databases(this.client);
+    this.bucket=new Storage(this.client);
+  }
+
+  async createPost({title,slug,content,featuredImage,status,userId}){
+    try{
+        return await this.Databases.createDocument(
+          conf.appwriteDatabaseId,
+          conf.appwriteCollectionId,slug,{
+            title,
+            content,
+            featuredImage,
+            status,
+            userId,
+          }
+          
+        )
+
+    }catch(error){
+      console.error("APPWRITE ERROR",error);
+    }
+  }
+
+  async updatePost(slug,{title,content,featuredImage,status}){
+   
+    try{
+      await this.Databases.updateDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        {
+          title,content,
+          featuredImage,
+          status
+        }
+      )
+} catch(error){
+      console.error("APPWRITE ERROR",error);
+    }
+  }
+  async deletePost(slug){
+    try{
+      await this.Databases.deleteDocument(conf.appwriteDatabaseId,conf.appwriteCollectionId,slug);
+      return true;
+    }catch(error){
+      console.error("APPWRITE ERROR",error);
+      return false;
+    }
+  }
+  async getPost(slug){
+    try{
+       return await this.Databases.getDocument(conf.appwriteDatabaseId,conf.appwriteCollectionId,slug);
+    }
+    catch(error){
+      console.error("APPWRITE ERROR",error);
+      return false;
+    }
+  }
+
+  async getPosts(queries=[Query.equal("status","active")]){
+    try{
+       return await this.Databases.listDocuments(conf.appwriteDatabaseId,conf.appwriteCollectionId,queries,)
+    }catch(error){
+      console.error("APPWRITE ERROR",error);
+      return false;
+  }
+}
+
+ async uploadFile(file){
+   try{
+       return await this.bucket.createFile(
+        conf.appwriteStorageId,ID.unique(),file
+       );
+   }catch(error){
+     console.error("APPWRITE ERROR",error);
+     return false;
+   }
+
+}
+
+async deleteFile(fileId){
+  try{
+    await this.bucket.deleteFile(conf.appwriteBucketId,fileId);
+    return true;
+  }catch(error){
+    console.error("APPWRITE ERROR",error);
+    return false;
+  }
+}
+
+getFilePreview(fileId){
+  return this.bucket.getFilePreview(conf .appwriteBucketId,fileId);
+}
+
+}
